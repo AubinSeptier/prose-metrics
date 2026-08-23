91e3a8b1a2c02e00115390913b6dd4f4490f288d
Aubin Septier <aubinseptier@gmail.com>
2026-08-23 18:42:41 +0200

    feat(metrics): add Style metrics calculation

--- prose_metrics/metrics/__init__.py
+++ prose_metrics/metrics/__init__.py
@@ -1,6 +1,7 @@
 """Metrics package containing extractors for text metrics."""
 
 from prose_metrics.metrics.rhythm import compute_rhythm_metrics
+from prose_metrics.metrics.style import compute_style_metrics
 from prose_metrics.metrics.volume import compute_volume_metrics
 
-__all__ = ["compute_rhythm_metrics", "compute_volume_metrics"]
+__all__ = ["compute_rhythm_metrics", "compute_style_metrics", "compute_volume_metrics"]
